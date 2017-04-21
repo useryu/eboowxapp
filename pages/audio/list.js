58261f569e5b3a0ed5dc327a8ef59b551d007f31
useryu <useryu@gmail.com>
2017-04-21 19:22:38 +0800

    audio list

--- pages/audio/list.js
+++ pages/audio/list.js
@@ -1,8 +1,45 @@
 // pages/audio/list.js
 Page({
-  data:{},
+  data: {
+    current: null,
+    audioAction: {
+      method: 'pause'
+    },
+    list:[]
+  },
   onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+    this.setData({
+      list:[
+        {
+      id:1,
+      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      name: '此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻',
+      author: '许巍1',
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    },
+    {
+      id:2,
+      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      name: '此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻',
+      author: '许巍2',
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    },
+    {
+      id:3,
+      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      name: '此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻',
+      author: '许巍3',
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    },
+    {
+      id:4,
+      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      name: '此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻此时此刻',
+      author: '许巍4',
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    }
+      ]
+    });
   },
   onReady:function(){
     // 页面渲染完成
@@ -15,5 +52,8 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  play:function(e){
+    var id=e.target.id
   }
 })
